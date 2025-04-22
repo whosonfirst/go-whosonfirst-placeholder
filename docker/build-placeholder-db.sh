fd1c4a70b62d9edb6cc67d01e4340c04295de2c3
thisisaaronland <thisisaaronland@localhost>
2025-04-22 14:12:56 -0700

    update docker stuff to bundle copy tool, working through bugs

--- docker/build-placeholder-db.sh
+++ docker/build-placeholder-db.sh
@@ -1,12 +1,33 @@
 #!/bin/sh
 
 DATABASE=/data/placeholder/store.sqlite3
+TARGET=
+ACCESS_TOKEN=
 
 ITERATOR_URI=org:///tmp?_exclude_alt=true&exclude=properties.edtf:deprecated=.*&dedupe=true
 
-/usr/local/bin/wof-extract-properties -iterator-uri "${ITERATOR_URI}" $@ > ${DATABASE}
+while getopts "A:T:" opt; do
+    # echo "-$opt = $OPTARG"
+    case "$opt" in
+	A)
+	    ACCCESS_TOKEN=$OPTARGS
+	    ;;
+	T)
+	    TARGET=$OPTARG
+	    ;;
+	: )
+	    echo "WHAT"
+	    ;;
+    esac
+done
+
+shift $((OPTIND-1))
+SOURCES=$@
+
+/usr/local/bin/wof-extract-properties -iterator-uri "${ITERATOR_URI}" -access-token "${ACCESS_TOKEN}" ${SOURCES} > ${DATABASE}
+# echo /usr/local/bin/wof-extract-properties -iterator-uri "${ITERATOR_URI}" -access-token "${ACCESS_TOKEN}" ${SOURCES} 
 
 cd /code/pelias/placeholder
 npm run build
 
-# copy /data/placeholder/store.sqlite3 to ... ??
+/usr/local/bin/copy file://${DATABASE} ${TARGET}
